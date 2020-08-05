d6165352782824d5371925cfc5d3a3d13b46d35b
MarkoG22 <marko.gacinovic.el@gmail.com>
2020-08-05 19:33:23 +0200

    MainWindow View and ViewModel

--- Nedeljni_1_Marko_Gacinovic/Company/MainWindow.xaml.cs
+++ Nedeljni_1_Marko_Gacinovic/Company/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Company.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Company
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel(this);
         }
     }
 }
---
+++ Nedeljni_1_Marko_Gacinovic/Company/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,118 @@
+﻿using Company.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Company.ViewModel
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        MainWindow main;
+
+        // properties for username and password
+        private string username;
+        public string Username
+        {
+            get { return username; }
+            set
+            {
+                username = value;
+                OnPropertyChanged("Username");
+            }
+
+        }
+
+        private string userPassword;
+        public string UserPassword
+        {
+            get { return userPassword; }
+            set
+            {
+                userPassword = value;
+                OnPropertyChanged("UserPassword");
+            }
+        }
+
+        // constructor
+        public MainWindowViewModel(MainWindow mainOpen)
+        {
+            main = mainOpen;
+        }
+
+        // command for the login button
+        private ICommand logIn;
+        public ICommand LogIn
+        {
+            get
+            {
+                if (logIn == null)
+                {
+                    logIn = new RelayCommand(param => SaveExecute(), param => CanSaveExecute());
+                }
+                return logIn;
+            }
+        }
+
+        private bool CanSaveExecute()
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// method for checking username and password and opening the windows
+        /// </summary>
+        private void SaveExecute()
+        {
+            if (username == "WPFMaster" && userPassword == "WPFAccess")
+            {
+                
+            }
+            else if (true)
+            {
+                
+            }
+            else
+            {
+                MessageBox.Show("Wrong username or password, please try again.");
+            }
+        }
+
+        // command for closing the window
+        private ICommand logout;
+        public ICommand Logout
+        {
+            get
+            {
+                if (logout == null)
+                {
+                    logout = new RelayCommand(param => CloseExecute(), param => CanCloseExecute());
+                }
+                return logout;
+            }
+        }
+
+        /// <summary>
+        /// method for closing the window
+        /// </summary>
+        private void CloseExecute()
+        {
+            try
+            {
+                main.Close();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Exception" + ex.Message.ToString());
+            }
+        }
+
+        private bool CanCloseExecute()
+        {
+            return true;
+        }
+    }
+}
