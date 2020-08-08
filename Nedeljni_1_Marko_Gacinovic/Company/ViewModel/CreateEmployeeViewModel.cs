3f3bbda4409b85a852e80bc9d4b7c9c7c33aa1f7
MarkoG22 <marko.gacinovic.el@gmail.com>
2020-08-08 14:08:03 +0200

    CreateManager View and ViewModel

---
+++ Nedeljni_1_Marko_Gacinovic/Company/ViewModel/CreateEmployeeViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Company.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.ViewModel
+{
+    class CreateEmployeeViewModel : ViewModelBase
+    {
+        CreateEmployeeView createEmployeeView;
+
+        public CreateEmployeeViewModel(CreateEmployeeView employeeOpen)
+        {
+            createEmployeeView = employeeOpen;
+        }
+    }
+}
--- Nedeljni_1_Marko_Gacinovic/Company/ViewModel/MainWindowViewModel.cs
+++ Nedeljni_1_Marko_Gacinovic/Company/ViewModel/MainWindowViewModel.cs
@@ -112,5 +112,55 @@ namespace Company.ViewModel
         {
             return true;
         }
+
+        private ICommand createEmployee;
+        public ICommand CreateEmployee
+        {
+            get
+            {
+                if (createEmployee == null)
+                {
+                    createEmployee = new RelayCommand(param => CreateEmployeeExecute(), param => CanCreateEmployeeExecute());
+                }
+                return createEmployee;
+            }
+            
+        }
+
+        private bool CanCreateEmployeeExecute()
+        {
+            return true;
+        }
+
+        private void CreateEmployeeExecute()
+        {
+            CreateEmployeeView employee = new CreateEmployeeView();
+            employee.ShowDialog();
+        }
+
+        private ICommand createManager;
+        public ICommand CreateManager
+        {
+            get
+            {
+                if (createManager == null)
+                {
+                    createManager = new RelayCommand(param => CreateManagerExecute(), param => CanCreateManagerExecute());
+                }
+                return createManager;
+            }
+
+        }
+
+        private bool CanCreateManagerExecute()
+        {
+            return true;
+        }
+
+        private void CreateManagerExecute()
+        {
+            CreateManagerView manager = new CreateManagerView();
+            manager.ShowDialog();
+        }
     }
 }
